45232dd7e660d03294f4fa7f8f536dcdea3dcd03
Mizunanari <54758260+Mizunanari@users.noreply.github.com>
2022-12-20 10:27:04 +0900

    Update create.sh

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -2,7 +2,7 @@
 
 # bash_completion
 sudo apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install --no-install-recommends bash-completion
+    && sudo apt-get -y install --no-install-recommends bash-completion
 printf "\n# bash_completion\n. /usr/share/bash-completion/bash_completion\n" >> ~/.bashrc
 
 # poetry
